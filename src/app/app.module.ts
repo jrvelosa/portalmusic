d2f8ce4f63c4aee7b28d5caeeb116cc0b6de81c4
Tatiana <Tatytoro2000@gmail.com>
2020-05-31 23:01:17 -0500

    Solucion de las tareas PM-3,4,5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './componentes/search/search.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ArtistaComponent } from './componentes/artista/artista.component';
+import { NavbarComponent } from './componentes/share/navbar/navbar.component';
+import { from } from 'rxjs';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    ArtistaComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/componentes/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  styles: [],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
