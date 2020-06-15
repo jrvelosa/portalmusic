fdf6aa96dd5e94bf6e97c098e3031ecddded1e59
andres <andres0259@hotmail.com>
2020-06-14 20:17:25 -0500

    Manejo de errores

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -5,16 +5,30 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styles: [],
 })
 export class HomeComponent {
   nuevasCanciones: any[] = [];
   loading: boolean;
+
+  error: boolean;
+  mensajeError: string;
+
   constructor(private spotify: SpotifyService) {
     this.loading = true;
-    this.spotify.getNewReleases().subscribe((data: any) => {
-      this.nuevasCanciones = data;
-      this.loading = false;
-    });
+    this.error = false;
+
+    this.spotify.getNewReleases().subscribe(
+      (data: any) => {
+        this.nuevasCanciones = data;
+        this.loading = false;
+      },
+      (errorServicio) => {
+        this.loading = false;
+        this.error = true;
+        console.log(errorServicio);
+        this.mensajeError = errorServicio.error.error.message;
+      }
+    );
   }
 }
